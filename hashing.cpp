fb1e205afa1d9157479ed5394eb29093799fccd5
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2022-11-05 14:13:53 +0100

    lab 3 (work in progress)

---
+++ hashing.cpp
@@ -0,0 +1,28 @@
+/*
+>hashing con strategia anti-collisione "chained resolution" (Lista doppiamente concatenata)
+>tipo List????
+>strutture dati dinamiche -> problema di continuous allocation/deallocation?
+>una soluzione: appena riempio una certa zona dealloco e rialloco con il doppio dello spazio
+>ciò avviene in automatico e viene fatto dal compilatore
+*/
+
+#include <iostream>
+using namespace std; 
+
+template <typename T>
+class DLLNode{
+
+};
+
+template <typename T>
+class DoubleLinkedList{
+
+};
+
+int hashing(){
+
+};
+
+int main(){
+
+}
